2ca42738899c5cf719e0f9e3b5a107824267ff47
Fernando Silva <85886086+FernandoS01@users.noreply.github.com>
2025-12-09 11:02:16 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -47,7 +47,7 @@ int main() {
 for(int x = 0; x < 10; x++){
     printf("\n");
     for(int y = 0; y < 10; y++){
-        printf("%i,",tabuleiro[x][y]);
+        printf("%i ",tabuleiro[x][y]);
     };
 };    
 
